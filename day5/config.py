e2238c7c68a36f1b810f4104c809124b30b1a2c8
varavite <vanaganiraviteja@gmail.com>
2025-09-16 14:00:28 +0530

    changes added

---
+++ day5/config.py
@@ -0,0 +1,3 @@
+from_address = "vanaganiraviteja111@gmail.com"
+to_address = "gmaheswaranmca@gmail.com"
+app_password = "bowq bvyu plnb tcee"  # generate in Google Account -> Security -> App passwords
